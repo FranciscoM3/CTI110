62d4668897ac3b45a4f43767ac2096d332d792e4
FranciscoM3 <47481393+FranciscoM3@users.noreply.github.com>
2019-02-09 12:22:12 -0500

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_FranciscoDavis.py
@@ -0,0 +1,16 @@
+# Projected sales amount and profit.
+# 02FEB2019
+# CTI-110 P2T1 - Sales Prediction
+# Francisco Davis
+#
+
+total_sales = float(input('Enter the projected sales:'))
+
+profit = total_sales * 0.23
+
+
+print('The profit is $', format(profit, ',.2f'))
+
+
+                                
+                                
