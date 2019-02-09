d9e9e8417bbccb4ad9ac8fa4297ee873ad5dc282
FranciscoM3 <47481393+FranciscoM3@users.noreply.github.com>
2019-02-09 12:27:23 -0500

    Update P2T1_SalesPrediction_FranciscoDavis.py

--- P2T1_SalesPrediction_FranciscoDavis.py
+++ P2T1_SalesPrediction_FranciscoDavis.py
@@ -4,12 +4,11 @@
 # Francisco Davis
 #
 
-total_sales = float(input('Enter the projected sales:'))
+total_sales = float(input('Enter the projected sales: $'))
 
 profit = total_sales * 0.23
 
-
-print('The profit is $', format(profit, ',.2f'))
+print('The profit is $', format(profit, ',.2f'), sep='')
 
 
                                 
