27ece29259752987b50fe7585f91a1633c83695b
FranciscoM3 <47481393+FranciscoM3@users.noreply.github.com>
2019-02-13 15:50:35 -0500

    Add files via upload
    Debug a nested decision structure that would determine grades based on a 10 point scale

---
+++ P3LAB_Davis.py
@@ -0,0 +1,40 @@
+# Debug an unfinished program.
+# Davis
+# 13FEB2019
+# CTI-110 P3LAB - Debugging
+#
+# Establish variables for grades.
+# Input grade.
+# Use Nested decision structure to determine grade.
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+   
+
+    
+    score = int(input('Enter grade: '))
+
+    if score >= A_score:
+        print('Your grade is: A')
+    else:
+        if score >= B_score:
+            print('Your grade is: B')
+        else:
+            if score >= C_score:
+                print('Your grade is: C')
+            else:
+                if score >= D_score:
+                    print('Your grade is: D')
+                else:
+                    print('Your grade is: F')
+
+
+
+# program start
+main()
